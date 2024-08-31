21ac93d2387653242c6cc555edb26a8fb4ebf0ab
AdiyZd <154992866+AdiyZd@users.noreply.github.com>
2024-08-31 12:26:44 +0700

    update

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="aset/foto.jpg">
+   <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>ABSENSI SANDIMEDIA</title>
+</head>
+<body>
+  <div class="nav-bar" >
+    <!-- navbar -->
+    <div class="collapse" id="navbarToggleExternalContent" data-bs-theme="dark">
+  <div class="bg-dark p-4">
+    <h5 class="text-body-emphasis h4">Sandi komputer</h5>
+    <span class="text-body-secondary">Websaite absensi di buat oleh Adi</span>
+  </div>
+</div>
+<nav class="navbar navbar-dark bg-dark">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </div>
+</nav>
+    <!-- navbar end -->
+    <div class="absensi">
+      <div class="input-group flex-nowrap">
+        <span class="input-group-text" id="addon-wrapping">@</span>
+        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping" id="username">
+        </div>
+        </div>
+  <!-- akhir dari input -->
+  <!-- ijin -->
+  <h2 class="Adiy_h2">ABSENSI SANDIMEDIA</h2>
+  <hr>
+  <!-- awak cek box -->
+  <div class="cb">
+    <div class="btn-group" role="group" aria-label="Basic outlined example">
+  <button type="button" class="btn btn-outline-primary">MASUK</button>
+  <button type="button" class="btn btn-outline-primary">SETENGAH HARI</button>
+  <button type="button" class="btn btn-outline-primary">SAKIT</button>
+</div>
+  </div>
+  <hr class="hr_2">
+  </div>
+  <!-- menu button -->
+  <div class="d-grid gap-2">
+  <button class="btn btn-primary" type="button" onclick="main()">KIRIM ABSENSI</button>
+</div>
+    <!-- ini adalah akhir -->
+<footer>
+        <p>&copy; 2024 Your Company. All rights reserved.</p>
+        <p>
+            Follow us on 
+            <a href="https://www.facebook.com" target="100035408843000">Facebook</a>, 
+            <a href="https://www.twitter.com" target="_blank">Twitter</a>, and 
+            <a href="https://www.instagram.com" target="xyd_adiy">Instagram</a>.
+        </p>
+    </footer>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <script src="Main.js"></script>
+</body>
+</html>
